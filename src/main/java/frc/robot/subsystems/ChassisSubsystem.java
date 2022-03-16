b9fe1c1140971fc6a1a0fa40a64152f60abbef22
Kevin Quan <kquan@werfen.com>
2022-03-16 09:47:19 -0400

    removed getScaledValue and use the differential drive's deadband and square inputs

--- src/main/java/frc/robot/subsystems/ChassisSubsystem.java
+++ src/main/java/frc/robot/subsystems/ChassisSubsystem.java
@@ -57,36 +57,6 @@ public class ChassisSubsystem extends SubsystemBase {
    */
   private static final double crawl = 0.3;
 
-    /**
-   * The minimum (closest to 0) speed controller command for the right side of the drive train to start moving forward. Must be empirically derived.
-   */
-  private static double mechDeadbandRightForward = 0.25;
-
-  /**
-   * The maximum (closest to 0) speed controller command for the right side of the drive train to start moving backward. Must be empirically derived.
-   * Must be negative.
-   */
-  private static double mechDeadbandRightBackward = -0.27;
-
-  /**
-   * The minimum (closest to 0) speed controller command for the left side of the drive train to start moving forward. Must be empirically derived.
-   */
-  private static double mechDeadbandLeftForward = 0.23;
-
-  /**
-   * The maximum (closest to 0) speed controller command for the left side of the drive train to start moving backward. Must be empirically derived.
-   * Must be negative.
-   */
-  private static double mechDeadbandLeftBackward = -0.29;
-
-  /**
-   * The minimum joystick value to actually send a command to the speed controller, to prevent noise near 0.
-   */
-  private static double softwareDeadband = 0.05;
-
-  //arcade drive constant
-  private double turnGain = 0.75;
-
   /**
    * The direction which is "forward"; 1 represents the hatch side and -1 represents the cargo side.
    */
@@ -142,7 +112,7 @@ public class ChassisSubsystem extends SubsystemBase {
    * @param rightspeed - The right joystick controller speed -1 to 1
    */
   public void tankDrive(double leftSpeed, double rightSpeed) {
-    driveTrain.tankDrive(leftSpeed, rightSpeed);
+    driveTrain.tankDrive(leftSpeed, rightSpeed, true);
   }
 
   /**
@@ -179,11 +149,11 @@ public class ChassisSubsystem extends SubsystemBase {
       double rightVal;
       double leftVal;
       if(dir == Constants.INTAKE_IN) {
-        rightVal = getScaledValue(driverOI.getJoystick().getRawAxis(Constants.RIGHT_JOYSTICK_Y), scale, RobotSide.RIGHT);
-        leftVal = getScaledValue(driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_Y), scale, RobotSide.LEFT);
+        rightVal = driverOI.getJoystick().getRawAxis(Constants.RIGHT_JOYSTICK_Y);
+        leftVal = driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_Y);
       } else {
-        rightVal = getScaledValue(driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_Y), scale, RobotSide.RIGHT);
-        leftVal = getScaledValue(driverOI.getJoystick().getRawAxis(Constants.RIGHT_JOYSTICK_Y), scale, RobotSide.LEFT);
+        rightVal = driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_Y);
+        leftVal = driverOI.getJoystick().getRawAxis(Constants.RIGHT_JOYSTICK_Y);
       }
       if(m_gyroAngle != Constants.GYRO_NOTUSED)
       {
@@ -201,61 +171,12 @@ public class ChassisSubsystem extends SubsystemBase {
         tankDrive(leftVal * speedMultiplier * dir, rightVal * speedMultiplier * dir);
       }
     } else if (driveTrainType == RobotContainer.DriveTrainType.ARCADE) {
-      double speed = getScaledValue(driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_Y), scale, RobotSide.LEFT) * dir;
-      double rotation = getScaledValue(driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_X), scale, RobotSide.RIGHT);
+      double speed = driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_Y) * dir;
+      double rotation = driverOI.getJoystick().getRawAxis(Constants.LEFT_JOYSTICK_X);
       SmartDashboard.putNumber("Speed", speed);
       SmartDashboard.putNumber("Rotation", rotation);
       arcadeDrive(speed, rotation);
     }
-  }
-
-  /**
-   * Find the scale factor based on the input value and scale on each side of the Robot's motors
-   *
-   * @param val
-   * @param scale
-   * @param side
-   * @return
-   */
-  private double getScaledValue(double val, int scale, RobotSide side) {
-    double forward, back;
-
-    if(side.equals(RobotSide.RIGHT)) {
-        forward = Math.abs(mechDeadbandRightForward);
-        back = Math.abs(mechDeadbandRightBackward);
-    } else {
-        forward = Math.abs(mechDeadbandLeftForward);
-        back = Math.abs(mechDeadbandLeftBackward);
-    }
-
-    if(Math.abs(val) < softwareDeadband) {
-        return 0;
-    } else if(scale == 0) {
-        return deadzone(val);
-    } else if(scale == 1) {
-        if(val > 0) {
-            return (((1.0 - forward) * (val - 1.0) / (1.0 - softwareDeadband)) + 1.0);
-        } else {
-            return (((1.0 - back) * (val + 1.0) / (1.0 - softwareDeadband)) -  1.0);
-        }
-    } else if(scale == 2) {
-        if(val > 0) {
-            return (1- forward) / Math.pow(1 - softwareDeadband, 2) * Math.pow(val - softwareDeadband, 2) + forward;
-        } else {
-            return (-1 + back) / Math.pow(-1 + softwareDeadband, 2) * Math.pow(val + softwareDeadband, 2) - back;
-        }
-    } else {
-        return 0;
-    }
-  }
-
-  /**
-   * Method to reduce noise around the 0 position of the joystick.
-   * @param val The raw joystick input.
-   * @return If the input is outside the range <code> (-softwareDeadband < val < softwareDeadband)</code>, it is returned. Else, 0 is returned.
-   */
-  private double deadzone(double val) {
-    return Math.abs(val) > softwareDeadband ? val : 0;
   }
 
   public double getGyroAngle() {
